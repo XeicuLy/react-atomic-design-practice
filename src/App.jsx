817521c4b8674fc6da4b6d40e6fdfb77a9a4070c
XeicuLy <106128913+XeicuLy@users.noreply.github.com>
2022-11-28 00:01:20 +0900

    :memo: add: atoms追加
    Atomic Designに際して、atomsのbuttonを追加しました。

--- src/App.jsx
+++ src/App.jsx
@@ -1,2 +1,13 @@
-const App = () => {};
+import { PrimaryButton } from './components/atoms/button/PrimaryButton';
+import { SecondaryButton } from './components/atoms/button/SecondaryButton';
+
+const App = () => {
+  return (
+    <>
+      <h1>Hello</h1>
+      <PrimaryButton>ボタン</PrimaryButton>
+      <SecondaryButton>検索</SecondaryButton>
+    </>
+  );
+};
 export default App;
